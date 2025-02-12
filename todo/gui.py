ad620b379b49c34313eb690f26785ab7f0da507c
Jenny Kao <jjjenny.kao@gmail.com>
2025-02-12 15:05:59 -0700

    Add in Edit feature

--- todo/gui.py
+++ todo/gui.py
@@ -1,6 +1,6 @@
 ## pip install FreeSimpleGUI -- third-party modules
 import functions 
-import FreeSimpleGUI as sg
+import PySimpleGUI as sg
 
 #sg.theme_previewer(); theme_name_list = sg.theme_list() print(theme_name_list)
 ## layout expect list of lists
@@ -8,42 +8,61 @@ import FreeSimpleGUI as sg
 
 ### create instance of the element
 sg.theme("LightBlue3") #Sandy Peach
-label = sg.Text("Enter in a To-Do Item")
-input_box = sg.InputText(tooltip="Enter todo", key="todo")
+
+enter_label = sg.Text("Enter To Do Item")
+input_box = sg.InputText(key="todo")
+
 add_button = sg.Button("Add")
-listBox = sg.Listbox(values=functions.get_todos(), key="todos",
-                     enable_events=True, size=[45,10])
+
+listBox = sg.Listbox(values=functions.get_todos(), 
+                     key="todos",
+                     enable_events=True, 
+                     size=[45,10])
+
 edit_button = sg.Button("Edit")
 
-layout = [[label],[input_box, add_button], [listBox,edit_button]]
+layout = [[enter_label],[input_box, add_button], [listBox, edit_button]]
 
 ### create a window instance
-window = sg.Window('My To-Do App',layout, 
-                   font =('Helvetica', 20))
+window = sg.Window('My To Do List',layout, font=('Helvetica', 20))
+
 while True:
     event, values= window.read()
-    match event:
+    print(1, event)
+    print(2, values)
+    print(3, values['todo'])
+    print(4, values['todos'])
 
+    match event:
         case 'Add':
             todos = functions.get_todos()
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
 
         case 'Edit':
             todo_to_edit = values['todos'][0]
             new_todo = values['todo']
 
             todos = functions.get_todos()
+            # replace the exsisting to do with new_todo
+            print(todos)
             index = todos.index(todo_to_edit)
+            print(index)
             todos[index] = new_todo
+            print(todos[index])
+            print(todos)
             functions.write_todos(todos)
-            window['todos'].update(value=todos)
-
+            window['todos'].update(values=todos)
+        
         case 'todos':
             window['todo'].update(value=values['todos'][0])
 
         case sg.WIN_CLOSED:
             break
-window.close() #close the program 
+
+window.close()
+
+#close the program 
 
