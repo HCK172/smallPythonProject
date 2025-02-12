f29b306407ec2f94ed0efc8cf3dd9be9eb914e46
Jenny Kao <jjjenny.kao@gmail.com>
2025-02-12 16:13:40 -0700

    inital comment - converter gui

--- converter/gui.py
+++ converter/gui.py
@@ -3,14 +3,25 @@ import FreeSimpleGUI as sg
 
 feetLabel = sg.Text("Enter feet")
 inchesLabel = sg.Text("Enter inches")
-inputFeet = sg.Input()
-inputInch = sg.Input()
+inputFeet = sg.Input(key="feet")
+inputInch = sg.Input(key="inch")
 convert = sg.Button("Convert")
+output_label = sg.Text(key="output")
 
 layout = [[feetLabel, inputFeet],
           [inchesLabel, inputInch],
-          [convert]]
+          [convert, output_label]]
 
 window = sg.Window("Convertor", layout)
-window.read()
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    totalInch = float(values["feet"])*12 + float(values["inch"])
+    meter = totalInch*0.0254
+    window["output"].update(value=f"{meter} m")
+
+sg.WIN_CLOSE()
+
 window.close()
